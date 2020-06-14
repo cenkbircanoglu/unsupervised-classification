33c3cd434842c6aeac0c588d186bc8d077a25897
Cenk Bircanoglu <cenk.bircanoglu@adevinta.com>
2020-06-14 13:23:48 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='unsupervised_classification',
+      version='0.1',
+      url='https://github.com/cenkbircanoglu/unsupervised-classification',
+      author='Cenk Bircanoglu',
+      author_email='cenk.bircanoglu@gmail.com',
+      packages=['src'])
